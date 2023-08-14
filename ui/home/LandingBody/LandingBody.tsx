525ede73217e79704d4df34c70379833c1adb201
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-08-14 16:04:17 +0400

    fix

--- ui/home/LandingBody/LandingBody.tsx
+++ ui/home/LandingBody/LandingBody.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Content from "../../../../Parazone/src/app/components/Content/Content";
-import About from "../../../../Parazone/src/app/components/About/About";
-import Info from "../../../../Parazone/src/app/components/Info/Info";
+import Content from "../../../../PARAZONE/src/app/components/Content/Content";
+import About from "../../../../PARAZONE/src/app/components/About/About";
+import Info from "../../../../PARAZONE/src/app/components/Info/Info";
 import RoadMap from "../../../src/app/components/RoadMap/RoadMap";
 import Global from "../../../src/app/components/Global/Global"
-import Popup from "../../../../Parazone/src/app/components/Pop-Up/PopUp"
+import Popup from "../../../../PARAZONE/src/app/components/Pop-Up/PopUp"
 function LandingBody() {
     return (
         <div className="w-full bg-white">
